cb544c19d1bcfc93eae3bb2afd774d8971d0964f
Sean Goresht <seangores@gmail.com>
2012-11-02 04:33:32 -0600

    Added compatability with IE7 and down + styling
    Improving bootstrap as I go.  Relative widths, consistent measurements
    in ems rather than px, other important changes including compatibility
    with IE7.
    
    Using htc.pie, IE7 and down will work with boxshadow and border-radius.
    In addition, using the icomoon script, all icons using the "content"
    selector (not before or after) can be viewed.  It's a good start, but
    there's still a lot more work to be done!

---
+++ tests/ui/js/script.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Sean
+ * Date: 11/1/12
+ * Time: 12:32 AM
+ * To change this template use File | Settings | File Templates.
+ */
+jQuery(document).ready(function ($)
+{
+	var $selects = $("select");
+	$selects.chosen();
+	//$("table").dataTable();
+});
